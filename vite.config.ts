a01a936889825bb250fb0263b5032fce67a39d58
KirstenSotelo <kirsten.sotelo@gmail.com>
2026-07-17 16:01:10 -0400

    Allowing vercel

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   server: {
     port: 5173,
-    open: true
+    open: true,
+    allowedHosts: [
+      'sb-59ow5ufj2u5q.vercel.run'
+    ]
   }
 })
